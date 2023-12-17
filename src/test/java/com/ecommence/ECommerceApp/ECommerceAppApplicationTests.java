28f3f855437fe65cf7849b6c975412fb27b656cb
master-shiffu <jheetasukhjindersingh@gmail.com>
2023-12-17 00:09:02 -0800

    Spring Boot Initialization for the project

---
+++ src/test/java/com/ecommence/ECommerceApp/ECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommence.ECommerceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
